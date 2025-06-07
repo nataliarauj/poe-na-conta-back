008704db726a5b2c6aa8c0ec04daba8cca116fb5
Alexxx20 <aleksanderlucena@gmail.com>
2025-06-07 14:43:43 -0300

    feat: added controllers and routes for gains, debts, total, categories and listing all gains and debts

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const viewController = require('../controllers/viewController');
+const auth = require('../middleware/auth')
 
 // Rota para buscar o resumo de saldo dos clientes
-router.get('/balancesummary', viewController.getBalanceSummary);
+router.get('/balanceTotal', auth, viewController.getBalanceTotal);
+
+router.get('/balanceGains', auth, viewController.getBalanceGains);
+
+router.get('/balanceDebts', auth, viewController.getBalanceDebts);
+
+router.get('/balanceCategories', auth, viewController.getBalanceCategories);
+
+router.get('/balanceAllDebts', auth, viewController.getBalanceAllDebts);
+
+router.get('/balanceAllGains', auth, viewController.getBalanceAllGains);
 
 module.exports = router;
