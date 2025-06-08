5eccbd59d1a683188df9e32e6c92e837bb3be915
nataliarauj <natalia.araujo08@aluno.ifce.edu.br>
2025-06-08 13:09:57 -0300

    feat: creation of update and delete functions and their respective routes

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -1,5 +1,6 @@
-const Transaction = require('../models/Transaction');
+const { Transaction } = require('../models');
 
+//Criar
 exports.create = async(req, res) => {
   const {category_id, balance, title, description, createdat} = req.body;
   try{
@@ -35,3 +36,66 @@ exports.create = async(req, res) => {
     res.status(500).json({ error: 'Erro interno ao criar transação.' });
   }
 }
+
+//Atualizar
+exports.update = async (req, res) => {
+  const { id } = req.params;
+  const { category_id, balance, title, description, createdat } = req.body;
+
+  try {
+    if (!category_id || !balance || !title) {
+      return res.status(400).json({ message: 'Preencha todos os campos obrigatórios' });
+    }
+
+    const client_id = req.user.id;
+
+    const transaction = await Transaction.findOne({
+      where: { id, client_id }
+    });
+
+    if (!transaction) {
+      return res.status(404).json({ message: 'Transação não encontrada!' });
+    }
+
+    const updateData = {
+      title,
+      category_id,
+      balance,
+    };
+
+    if (description !== undefined) updateData.description = description;
+    if (createdat !== undefined) updateData.createdat = createdat;
+
+    await transaction.update(updateData);
+
+    res.status(200).json({ message: 'Transação atualizada com sucesso!' });
+
+  } catch (error) {
+    console.error('Erro ao atualizar transação: ', error);
+    res.status(500).json({ error: 'Erro interno ao atualizar transação' });
+  }
+};
+
+//Deletar
+exports.delete = async (req, res) => {
+  const { id } = req.params;
+  const client_id = req.user.id;
+
+  try {
+    const transaction = await Transaction.findOne({
+      where: { id, client_id }
+    });
+
+    if (!transaction) {
+      return res.status(404).json({ message: 'Transação não encontrada!' });
+    }
+
+    await transaction.destroy();
+
+    res.status(200).json({ message: 'Transação deletada com sucesso!' });
+
+  } catch (error) {
+    console.error('Erro ao deletar transação:', error);
+    res.status(500).json({ error: 'Erro interno ao deletar transação' });
+  }
+};
