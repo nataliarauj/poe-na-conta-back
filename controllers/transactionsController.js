b4a4b2fa4966795620671b5f5a7ca0cee3a03287
Alexxx20 <aleksanderlucena@gmail.com>
2025-06-08 11:10:46 -0300

    feat: added create transactiom

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -1,44 +1,37 @@
 const Transaction = require('../models/Transaction');
 
-// Criar
-exports.create = async (req, res) => {
-  try {
-    const transaction = await Transaction.create(req.body);
-    res.status(201).json(transaction);
-  } catch (err) {
-    res.status(400).json({ error: 'Erro ao criar transação', details: err.message });
-  }
-};
-
-// Atualizar
-exports.update = async (req, res) => {
-  try {
-    const [updated] = await Transaction.update(req.body, {
-      where: { id: req.params.id }
-    });
-    if (updated) {
-      const transaction = await Transaction.findByPk(req.params.id);
-      res.json(transaction);
-    } else {
-      res.status(404).json({ error: 'Transação não encontrada' });
+exports.create = async(req, res) => {
+  const {category_id, balance, title, description, createdat} = req.body;
+  try{
+    
+    if(!category_id || !balance || !title){
+      return res.status(400).json({message: 'Preencha todos os campos obrigatórios'})
     }
-  } catch (err) {
-    res.status(400).json({ error: 'Erro ao atualizar transação' });
-  }
-};
-
-// Deletar
-exports.delete = async (req, res) => {
-  try {
-    const transaction = await Transaction.destroy({
-      where: { id: req.params.id }
-    });
-    if (transaction) {
-      res.json({ message: 'Transação deletada com sucesso' });
-    } else {
-      res.status(404).json({ error: 'Transação não encontrada' });
+    const client_id = req.user.id
+    
+    const transactionBody ={
+      title,
+      category_id,
+      balance,
+      client_id
     }
-  } catch (err) {
-    res.status(500).json({ error: 'Erro ao deletar transação' });
+    
+    if(description){
+      transactionBody.description = description
+    }
+
+    if(createdat){
+      transactionBody.createdat = createdat
+    }
+
+    const newTransaction = await Transaction.create(transactionBody)
+    
+    res.status(201).json({ 
+      message: 'Transação criada com sucesso'
+    });
+
+  }catch (error) {
+    console.error('Erro ao criar transação:', error);
+    res.status(500).json({ error: 'Erro interno ao criar transação.' });
   }
-};
+}
