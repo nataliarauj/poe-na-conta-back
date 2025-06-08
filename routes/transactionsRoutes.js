64c90ee1a727d00c8c59ba65029a297d02e1a8d3
Alexxx20 <aleksanderlucena@gmail.com>
2025-06-08 11:10:46 -0300

    feat: added create transactiom

--- routes/transactionsRoutes.js
+++ routes/transactionsRoutes.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const transactionsController = require('../controllers/transactionsController');
+const auth = require('../middleware/auth')
 
 // POST /transactions - Fazer uma nova transação
-router.post('/transactions', transactionsController.create);
+router.post('/add-transaction', auth, transactionsController.create);
 
 // PUT /transactions/:id - Atualização de transação
-router.patch('/transactions/:id', transactionsController.update);
+// router.patch('/transactions/:id', transactionsController.update);
 
 // DELETE /transactions/:id - Deletar transação específica
-router.delete('/transactions/:id', transactionsController.delete);
+// router.delete('/transactions/:id', transactionsController.delete);
 
 module.exports = router;
